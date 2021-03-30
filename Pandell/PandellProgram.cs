d834b5d8cf3a3e8d6289de0aeca05c001156a287
Mort09 <matthew.macmaster@routeique.com>
2021-03-29 22:12:42 -0600

    Add project files.

---
+++ Pandell/PandellProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Pandell
+{
+    class PandellProgram
+    {
+        static void Main(string[] args)
+        {
+            //generates a list of 10,000 numbers in random order each time it is run. 
+            // Each number in the list must be unique and be between 1 and 10,000(inclusive)
+
+            //All numbers in the list are present and only oncem, so we can simply create the 1 to 10,000 list
+            // and promply randomize the organized list.
+            const int listSize = 10000;
+            var rand = new Random();
+            //Using a random number to order the list by, and randomizing the list that contains all the values needed.
+            var randomList = Enumerable.Range(1, listSize).OrderBy(e => rand.Next()).ToList();
+
+            var listOutput = string.Join(", ", randomList);
+            Console.WriteLine(listOutput);
+        }
+    }
+}
